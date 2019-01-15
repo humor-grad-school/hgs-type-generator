55889eefd4227732c65dafa2246a7312d6561674
skatpgusskat <skatpgusskat@naver.com>
2019-01-15 21:06:48 +0900

    Run generate before start watch

--- watch.js
+++ watch.js
@@ -3,15 +3,22 @@ const execAsync = require('./utils/execAsync');
 
 let isRunning = false;
 
-fs.watch(__dirname, async (event, filename) => {
+async function runGenerate() {
   if (isRunning) {
     return;
   }
+
+  isRunning = true;
+  console.log('generate!');
+  await execAsync('node generateAll');
+  isRunning = false;
+}
+
+fs.watch(__dirname, async (event, filename) => {
   if (filename === 'apiDefinitions.yml'
     || (filename.startsWith('generate') && filename.endsWith('.js'))) {
-    isRunning = true;
-    console.log('generate!');
-    await execAsync('node generateAll');
-    isRunning = false;
+    await runGenerate();
   }
-})
+});
+
+runGenerate();
